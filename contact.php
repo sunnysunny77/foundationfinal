f982dcb543203a408d12e1502ddf63daede2ed6c
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-02-14 19:59:25 +0800

    Initial commit

---
+++ contact.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Georges Garden Shop</title>
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="css/icons/foundation-icons.css">
+</head>
+
+<body>
+    
+    <div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
+
+        <button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
+        <div class="title-bar-title">Menu</div>
+
+    </div>
+
+    <header class="top-bar" id="responsive-menu">
+
+        <div class="top-bar-left">
+
+            <ul class="grid-x text-center vertical medium-horizontal
+                        menu" data-dropdown-menu>
+                <li class="cell small-6 medium-2 large-1 menu-text
+                            align-self-middle
+                            show-for-medium"><span class="hide">Georges Garden Shop</span></li>
+                <li class="cell small-6 medium-2 medium-offset-2 large-1
+                            large-offset-7
+                            align-self-middle">
+                    <a href="./index.html">HOME</a>
+                </li>
+                <li class="cell small-6 medium-2 large-1
+                            align-self-middle">
+                    <h1><a href="./contact.php">CONTACT</a></h1>
+                </li>
+                <li class="cell small-6 medium-2 large-1
+                            align-self-middle"><a href="./gallery.html">GALLERY</a></li>
+                <li class="cell small-6 medium-2 large-1
+                            align-self-middle"><a href="./video.html">VIDEO</a></li>
+            </ul>
+
+        </div>
+    
+    </header>
+
+    <main class="grid-container">
+
+        <h2 class="margin-bottom-3 text-center medium-text-right">Lorem ipsum</h2>
+
+        <i class="fi-clipboard"></i>
+
+        <p class="text-center">Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.</p>
+
+        <?php require_once "./form_php/form.php"; ?>
+
+        <div class="grid-x align-center text-center">
+            <blockquote class="cell small-10 medium-6">
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit.
+                <cite>Lorem</cite>
+            </blockquote>
+        </div>
+
+    </main>
+
+    <footer class="grid-x grid-padding-x align-justify">
+
+        <ul class="cell shrink align-self-middle no-bullet inline-list">
+            <li>
+                <i class="fi-link"></i>
+                <ul class="vertical menu text-left">
+                    <li><a href="./index.html">HOME</a></li>
+                    <li><a href="./contact.php">CONTACT</a></li>
+                    <li><a href="./gallery.html">GALLERY</a></li>
+                    <li><a href="./video.html">VIDEO</a></li>
+                </ul>
+            </li>
+        </ul>
+
+        <ul class="cell no-bullet inline-list shrink
+        align-self-middle text-center">
+            <li>Georges Garden Shop</li>
+            <li>&copy; D.C</li>
+        </ul>
+
+    </footer>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+</body>
+
+</html>
---
+++ form_php/form.php
@@ -0,0 +1,139 @@
+<?php
+
+session_start();
+
+if (!isset($_POST["action"])) {
+
+    echo '
+    <span class="badge primary text-right">1</span>
+    <span class="badge secondary">2</span>
+    <span class="badge secondary">3</span>
+    <form method="post" action="?">
+                <label>Purchase one or more products to proceed
+                    <select size="10" name="product[]" multiple>
+                        <option value="product-1">Product-1</option>
+                        <option value="product-2">Product-2</option>
+                        <option value="product-3">Product-3</option>
+                        <option value="product-4">Product-4</option>
+                        <option value="product-5">Product-5</option>
+                        <option value="product-6">Product-6</option>
+                        <option value="product-7">Product-7</option>
+                        <option value="product-8">Product-8</option>
+                        <option value="product-9">Product-9</option>
+                        <option value="product-10">Product-10</option>
+                        <option value="product-11">Product-11</option>
+                        <option value="product-12">Product-12</option>
+                        <option value="product-13">Product-13</option>
+                        <option value="product-14">Product-14</option>
+                        <option value="product-15">Product-15</option>
+                        <option value="product-16">Product-16</option>
+                        <option value="product-17">Product-17</option>
+                        <option value="product-18">Product-18</option>
+                        <option value="product-19">Product-19</option>
+                        <option value="product-20">Product-20</option>
+                    </select>
+                </label>
+                <div class="grid-x align-center input-group">
+                    <div class="input-group-button">
+                        <input name="action" type="submit" class="button" value="Submit">
+                    </div>
+                </div>
+            </form>';
+}
+
+if (isset($_POST["action"]) && $_POST["action"] == "Submit") {
+
+    $_SESSION["product"] = $_POST["product"];
+    echo "<span class=\"badge primary\">1</span>
+        <span class=\"badge primary\">2</span>
+        <span class=\"badge secondary\">3</span>";
+    foreach ($_POST["product"] as $key => $product) {
+        echo "
+      
+        <form method=\"post\" action=\"?\">
+        <div class=\"grid-x\">
+                <fieldset class=\"large-5 cell\">
+                <legend>Choose your quantity</legend>
+                    <div class=\"medium-6 cell\">
+                        <label>
+                        $product 
+                        <input name=\"$key\" type=\"number\" min=\"1\" value=\"1\">
+                        </label>
+                    </div>
+                </fieldset> 
+        </div>";
+    }
+
+    echo '<div class="medium-6 cell">
+            <label>Name
+              <input name="name" type="text">
+            </label>
+          </div>
+          <div class="medium-6 cell">
+            <label>email
+                <input name="email" type="email" >
+            </label>
+        </div>
+         <div class="medium-6 cell">
+            <label>Phone
+                <input name="phone" type="tel" >
+            </label>
+        </div>
+        <label>
+            Comments
+        <textarea name="comments"></textarea>
+        </label>
+        <div class="grid-x align-center input-group">
+            <div class="input-group-button">
+                <input name="action" type="submit" class="button" value="Order">
+            </div>
+        </div>
+</form>';
+}
+
+if (isset($_POST["action"]) && $_POST["action"] == "Order") {
+
+    echo "
+    <span class=\"badge primary\">1</span>
+    <span class=\"badge primary\">2</span>
+    <span class=\"badge primary\">3</span>
+    <div class=\"callout\">
+
+    <h3>Order</h3>
+
+    <p>
+
+    Name: 
+    {$_POST['name']}
+    <br/>
+    Email: 
+    {$_POST['email']}
+    <br/>
+    Phone: 
+    {$_POST['phone']}
+    <br/>
+    Comments: 
+    {$_POST['comments']}
+
+    </p>
+    <table>
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Quantity</th>
+      </tr>
+    </thead>
+    <tbody>";
+
+    foreach ($_SESSION["product"] as $key => $product) {
+
+        echo " <tr>
+            <td>$product</td>
+            <td>$_POST[$key]</td>
+        </tr>";
+    }
+
+    echo "</tbody>
+    </table>
+  </div>";
+}
