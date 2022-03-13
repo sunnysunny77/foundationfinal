c4d526130f43cd0e02fcb9a52145ea2e1ea4d4e9
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-03-13 12:23:31 +0800

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,28 +1,30 @@
-$('#form').submit(function (e) {
-    e.preventDefault();
-    const form = $(this);
-    const url = form.attr('action');
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: form.serialize(),
-        success: function (response) {
-            $("#response").html(response);
-            $('#form').submit(function (e) {
-                e.preventDefault();
-                const form = $(this);
-                const url = form.attr('action');
-                $.ajax({
-                    type: "POST",
-                    url: url,
-                    data: form.serialize(),
-                    success: function (response) {
-                        $("#response").html(response);
-                        window.scrollTo(0, 0);
-                    }
+$(document).ready(function() { 
+    $('#form').submit(function (e) {
+        e.preventDefault();
+        const form = $(this);
+        const url = form.attr('action');
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: form.serialize(),
+            success: function (response) {
+                $("#response").html(response);
+                $('#form').submit(function (e) {
+                    e.preventDefault();
+                    const form = $(this);
+                    const url = form.attr('action');
+                    $.ajax({
+                        type: "POST",
+                        url: url,
+                        data: form.serialize(),
+                        success: function (response) {
+                            $("#response").html(response);
+                            window.scrollTo(0, 0);
+                        }
+                    });
                 });
-            });
-        }
+            }
+        });
     });
 });
 
